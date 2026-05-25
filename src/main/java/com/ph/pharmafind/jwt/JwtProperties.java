bb8812891c5cab397993da2f5669dc715017841c
essoungou.wonga <hermanessoungou@gmail.com>
2026-05-25 16:18:44 +0100

    add jwtDecoder bean

--- src/main/java/com/ph/pharmafind/jwt/JwtProperties.java
+++ src/main/java/com/ph/pharmafind/jwt/JwtProperties.java
@@ -1,11 +1,6 @@
 package com.ph.pharmafind.jwt;
 
-import java.nio.file.Path;
-import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app.jwt")
-public record JwtProperties(
-    String issuer, Duration accessTokenTtl, Duration refreshTokenTtl, RsaKeys rsa) {
-  public record RsaKeys(Path privateKeyPath, Path publicKeyPath) {}
-}
+public record JwtProperties(String jwkSetUri) {}
